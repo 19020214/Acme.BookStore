f7d2b35e0c0d17cd4b395ced0e829749ea8e24e5
100049 <anhtq35@fpt.com.vn>
2022-08-05 09:50:18 +0700

    Add project files.

---
+++ test/Acme.BookStore.Domain.Tests/Authors/AuthorManager_Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Acme.BookStore.Books;
+using NSubstitute;
+using Shouldly;
+using Volo.Abp;
+using Volo.Abp.Domain.Repositories;
+using Xunit;
+
+namespace Acme.BookStore.Authors
+{
+    public class AuthorManager_Tests
+    {
+        [Fact]
+        public async Task Should_Able_To_ChangeName()
+        {
+            // Arrange
+
+            var _authorRepository = Substitute.For<IAuthorRepository>();
+            //_authorRepository.ClearReceivedCalls();
+            var _authorManager = new AuthorManager(_authorRepository);
+
+            var author = await _authorRepository.InsertAsync(
+                await _authorManager.CreateAsync(
+                    "SuperTest",
+                    new DateTime(1903, 06, 25),
+                    "Orwell produced literary criticism and poetry"
+                )
+            );
+            string name = "New SuperName";
+            // Act
+            await _authorManager.ChangeNameAsync(author, name);
+
+            //Assert
+            author.Name.ShouldBe(name);
+
+
+        }
+    }
+}
---
+++ test/Acme.BookStore.Domain.Tests/Books/Book_Test.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Acme.BookStore.Books
+{
+    public class Book_Test
+    {
+        [Fact]
+        public void CanChangeBookName()
+        {
+
+            //Arrange
+            var books = new Book
+            {
+                Name = "Test",
+                Price = 15.0f
+            };
+
+            //Act
+            books.Name = "New Name";
+
+            //Assert
+            Assert.Equal("New Name", books.Name);
+        }
+
+        [Fact]
+        public void CanChangeBookPrice()
+        {
+
+            //Arrange
+            var books = new Book
+            {
+                Name = "Test",
+                Price = 15.0f
+            };
+
+            //Act
+            books.Price = 20.0f;
+
+            //Assert
+            Assert.Equal(15.0f, books.Price);
+        }
+
+    }
+}
